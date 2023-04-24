5284b3f314b9a82d7077c7a234252a057051ea4d
Amit Vishwakarma <amit200698@gmail.com>
2023-04-25 04:14:13 +0530

    here we have create movie search website using api

--- index.js
+++ index.js
@@ -1,37 +1,69 @@
-const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': '6c44de9273msh0850216d0ca870fp14ceeejsnc7d5412f7757',
-		'X-RapidAPI-Host': 'imdb8.p.rapidapi.com'
-	}
-};
+const API_KEY = '11d8110a859024cfdd19c79b8822d6ac';
+const BASE_URL = 'https://api.themoviedb.org/3';
+const API_URL = `${BASE_URL}/discover/movie?sort_by=popularity.desc&api_key=${API_KEY}`;;
+const IMG_URL = 'https://image.tmdb.org/t/p/w500';
+const searchURL = BASE_URL + '/search/movie?api_key=' + API_KEY;
 
-//async await
-async function getData(url) {
-	try {
-		localStorage.clear();
-		let response = await fetch(url, options);
-		let data = await response.json();
-		console.log(data);
-		if (data.d.length)
-			ProcessData(data.d);
-	}
-	catch (ex) {
-		console.log(ex);
-	}
+const main = document.getElementById('main');
+const form = document.getElementById('form');
+const Search = document.getElementById('search');
+
+
+getMovies(API_URL);
+
+async function getMovies(url){
+    fetch(url).then(res => res.json()).then(data =>{
+        console.log(data);
+        showMovies(data.results);
+    });
+
 }
 
-getData('https://imdb8.p.rapidapi.com/auto-complete?q=Harry%20Potter');
+function showMovies(data){
+    main.innerHTML = '';
 
+    data.forEach(movie => {
+        const {poster_path, original_title, vote_average , overview} = movie;
+        const movieEl = document.createElement('div');
+        movieEl.setAttribute('id', 'movie');
+        movieEl.innerHTML = `<div id="movie">
+        <img src="${IMG_URL + poster_path}" alt="${original_title}">
 
+            <div id="movie-info">
+                <h3>${original_title}</h3>
+                <span class="${getColor(vote_average)}">${vote_average}</span>
+            </div>
 
-function ProcessData(info) {
-	// console.log(info)
-	for (let i = 0; i < info.length; i++) {
-		if (info[i].i) {
-			localStorage.setItem(`Image-${i+1}`,info[i].i.imageUrl );
-			// console.log(info[i].i.imageUrl);
-		}
-	}
-	abc();
+            <div id="overview">
+                <h3>overview</h3>
+                ${overview}
+            </div>
+        </div>`
+        
+
+        main.appendChild(movieEl);
+    });
 }
+
+function getColor(vote){
+    if(vote>=8){
+        return 'green'
+    }else if(vote>=5){
+        return 'orange'
+    }else{
+        return 'red'
+    }
+}
+
+form.addEventListener('submit', (e)=>{
+    e.preventDefault();
+
+    const searchTerm = Search.value;
+
+    if(searchTerm){
+        getMovies(searchURL+'&query='+searchTerm);
+    }
+    else{
+        getMovies(API_URL);
+    }
+})
--- index2.js
+++
@@ -1,6 +0,0 @@
-function abc(){
-    console.log(localStorage.length)
-    for(var i=0;i<localStorage.length;i++){
-        console.log(localStorage.getItem(`Image-${i+1}`))
-    }
-}
